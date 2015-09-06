0e759780a5703e383c9f430aaf937bb33b4c800f
spawner999 <lore.ferrario@gmail.com>
2015-09-06 15:53:06 +0200

    Json added in Builder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,83 @@
-var formattedName= HTMLheaderName.replace("%data%", "Lorenzo Ferrario");
-var formattedRole= HTMLheaderRole.replace("%data%", "FE Developer");
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
+var work = {
+	"jobs" : [
+		{
+			"employer" : "LineaEffe Srl",
+			"title" : "Labourer",
+			"location" : "Italy",
+			"dates" : "2015",
+			"description" : "Simple Labourer"
+		},
+		{
+			"employer" : "Università Insubria",
+			"title" : "Front Office",
+			"location" : "Italy",
+			"dates" : "2014",
+			"description" : "Student Helpdesk"
+		}
+	]
+};
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Udacity FEND P0",
+			"dates" : "08/15",
+			"description" : "About Me",
+			"images" : "myImgUrl1"
+		}
+		{
+			"title" : "Udacity FEND P1",
+			"dates" : "09/15",
+			"description" : "Build A Porfolio Site",
+			"images" : "myImgUrl2"
+		}
+		{
+			"title" : "Udacity FEND P2",
+			"dates" : "09/15",
+			"description" : "Online Resume",
+			"images" : "myImgUrl3"
+		}
+	]
+};
+
+var bio = {
+	"name" : "Lorenzo",
+	"role" : "FE Apprentice",
+	"welcomeMessage" : "Welcome to My Online Resume!",
+	"contacts" : {
+		"mobile" : "3495141142",
+		"email" : "lore.ferrario@gmail.com",
+		"github" : "spawner999",
+		"location" : "Italy"
+	},
+	"skills" : ["HTML5",
+		"CSS3",
+		"JS"]
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Liceo Scientifico Da Vinci",
+			"location" : "Gallarate, VA",
+			"degree" : "Diploma di Maturità",
+			"majors" : [null],
+			"dates" : "2005-2010"
+		},
+		{
+			"name" : "Università Insubria",
+			"location" : "Varese",
+			"degree" : "Not finished",
+			"majors" : ["Computer Science"],
+			"dates" : "2011/2014"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Front End Nanodegree",
+			"school" : "Udacity",
+			"dates" : "08-12/2015",
+			"url" : "http://www.udacity.com"
+		}
+	]
+}
